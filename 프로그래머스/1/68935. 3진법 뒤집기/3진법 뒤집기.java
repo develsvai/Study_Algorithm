62efff554e9c3b6b3ef0cde44a5b118b81e85226
하루이 <125961256+develsvai@users.noreply.github.com>
2024-04-09 20:37:39 +0900

    Update 3진법 뒤집기.java

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -3,10 +3,10 @@ class Solution {
         String str = "";
 
         while (n != 0) {
-            str += n % 3;
-            n /= 3;
+            str += n % 3; //10진을 3진으로 변환
+            n /= 3; // 3으로 나누어 수를 뒤집음
         }
         
-        return Integer.parseInt(str, 3);
+        return Integer.parseInt(str, 3); //10진으로 반환
     }
 }
